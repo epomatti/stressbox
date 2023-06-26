2423e593e585efda9cfa8253632c748fe3c92b7a
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-06-26 20:26:16 -0300

    golang base

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Starting server...")
+
+	port := flag.Int("port", 80, "The port on which to register the HTTP listener to.")
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "OK")
+	})
+
+	http.HandleFunc("/envs", func(w http.ResponseWriter, r *http.Request) {
+		env := r.URL.Query().Get("env")
+		value := os.Getenv(env)
+		log.Println(value)
+		fmt.Fprint(w, value)
+	})
+
+	http.HandleFunc("/fib", func(w http.ResponseWriter, r *http.Request) {
+		// Size
+		size := r.URL.Query().Get("size")
+		i, err := strconv.Atoi(size)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("You need to inform the size. Value received: " + size))
+			return
+		}
+		// Print
+		printStr := r.URL.Query().Get("print")
+		if len(printStr) == 0 {
+			printStr = "FALSE"
+		}
+		b, errPrint := strconv.ParseBool(printStr)
+		if errPrint != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("You need to inform the print boolean. Value received: " + printStr))
+			return
+		}
+
+		// Calculate
+		ui := uint(i)
+		f := fib(ui)
+		if b {
+			fmt.Println(strconv.FormatUint(uint64(f), 10))
+		}
+		fmt.Fprint(w, f)
+	})
+
+	addr := fmt.Sprintf(":%d", *port)
+	http.ListenAndServe(addr, nil)
+}
+
+func fib(n uint) uint {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return fib(n-1) + fib(n-2)
+	}
+}
