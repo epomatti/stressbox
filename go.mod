ddc9928f6800e70110a775b8e42981c12e868f99
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-20 13:07:27 -0300

    github module name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module github.com/epomatti/stressbox
 
 go 1.21
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"main/handlers"
+	"github.com/epomatti/stressbox/handlers"
 )
 
 func main() {
