8c489289af84715627813aa408ae1f0f08ab8414
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-20 00:18:19 -0300

    cpu handler test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -21,6 +21,22 @@ func TestOk(t *testing.T) {
 		t.Errorf("Error: %v", err)
 	}
 	if string(data) != expected {
-		t.Errorf("Expected Hello john but got %v", string(data))
+		t.Errorf("Expected [%v] but got [%v]", expected, string(data))
+	}
+}
+
+func TestCpu(t *testing.T) {
+	expected := "832040"
+	req := httptest.NewRequest(http.MethodGet, "/cpu?x=30", nil)
+	w := httptest.NewRecorder()
+	Cpu(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if string(data) != expected {
+		t.Errorf("Expected [%v] but got [%v]", expected, (data))
 	}
 }
